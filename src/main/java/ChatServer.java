5b87fbfd5e89a15b3260a3eaeb8acb3b1204c48a
aimov6464 <kozhagulov.raimbek2@gmail.com>
2023-12-22 17:15:45 +0600

    refactoring

--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class ChatServer {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChatServer {
+    private ServerSocket serverSocket;
+    private List<PrintWriter> allUsers;
+
+    public ChatServer() {
+        try {
+            serverSocket = new ServerSocket(5555);
+            allUsers = new ArrayList<>();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void processor() {
+        while (true) {
+            try {
+                System.out.println("Listening mode...>>>");
+                new Thread(new ChatThread(serverSocket.accept(), allUsers)).start();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+                break;
+            }
+        }
+        if (serverSocket != null) {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new ChatServer().processor();
+    }
 }
--- src/main/java/ChatThread.java
+++ src/main/java/ChatThread.java
@@ -1,2 +1,76 @@
-package PACKAGE_NAME;public class ChatThread {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.List;
+
+public class ChatThread implements Runnable {
+    private final Socket socket;
+    private PrintWriter writer;
+    private BufferedReader reader;
+    private boolean running;
+    private final List<PrintWriter> allUsers;
+    private String username;
+
+    public ChatThread(Socket socket, List<PrintWriter> otherUsers) {
+        this.socket = socket;
+        this.allUsers = otherUsers;
+        try {
+            writer = new PrintWriter(socket.getOutputStream());
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            running = true;
+            this.allUsers.add(writer);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        writer.println("Enter your name");
+        writer.flush();
+        username = this.getText();
+        while (running) {
+            String message = this.getText();
+            if (message == null) {
+                running = false;
+            } else {
+                this.broadCastMessage(message);
+            }
+        }
+        this.allUsers.remove(allUsers.indexOf(writer));
+        this.closeCloseable(reader);
+        this.closeCloseable(writer);
+        this.closeCloseable(socket);
+    }
+
+    private void closeCloseable(AutoCloseable closeable) {
+        if (closeable != null) {
+            try {
+                closeable.close();
+            } catch (Exception e) {
+                System.out.printf(e.getMessage());
+            }
+        }
+    }
+
+    private void broadCastMessage(String message) {
+        for (PrintWriter w : allUsers) {
+            if (!w.equals(writer)) {
+                w.println(username + ": " + message);
+                w.flush();
+            }
+        }
+    }
+
+    private String getText() {
+        String response = null;
+        try {
+            response = reader.readLine();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+        return response;
+    }
 }
